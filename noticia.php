c4abe058bc040d6c20a024c63ecce6b9fc09c365
Stchigel Ricci <Alumno26.stchigel.grimaldi.nicolas@ipm.edu.ar>
2024-09-12 12:43:26 -0300

    12/09/2024

--- noticia.php
+++ noticia.php
@@ -39,13 +39,17 @@
   </header>
   <?php 
         $id = $_GET["id"];
-        $res = mysqli_query($conexion,"select idnoticias, img, titulo from noticias where idnoticias=$id;");
+        $resp = mysqli_query($conexion,"select img, titulo, nbody from noticias where idnoticias=$id;");
+        while($res=mysqli_fetch_assoc($resp)){
             ?>
     <div class="headernoti">
         <div class="notimg"><img src="img/Francella.jpg" alt=""></div>
         <h1><?php echo $res['titulo']?></h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, excepturi eligendi officia aperiam atque eius quia doloribus maiores. Repudiandae tempora vero sint fuga aliquam harum consequuntur fugit? Sed, distinctio molestiae neque cumque fugit ipsa placeat? Minus rerum nisi placeat commodi accusamus consequatur, aliquam vero maiores! Doloremque, illo! Nihil aspernatur nam deserunt iusto enim culpa neque corporis, in accusantium eos soluta. Nemo perferendis voluptatem ipsa esse voluptate incidunt debitis libero, corrupti, maiores sapiente itaque illo suscipit, obcaecati nesciunt minima aut optio dolores officia. Nemo ex quidem, exercitationem consequatur deserunt maxime harum quisquam itaque saepe voluptas, totam quae. Recusandae voluptatibus deleniti tempore natus quibusdam ullam voluptatum, quos illum iste incidunt illo eligendi alias accusantium unde impedit libero, cupiditate veniam cumque, sit amet architecto ut ad sunt. Nemo temporibus eius adipisci, delectus possimus consectetur nisi excepturi quis facilis repudiandae. Iusto, error. Libero, reprehenderit. Modi ab quaerat id impedit veritatis. Labore, suscipit fugit tempora temporibus unde, quibusdam vitae a nulla magnam eum, eveniet blanditiis sapiente. Autem laborum expedita unde quia! Aperiam sed impedit provident, sequi atque laborum animi iure a perspiciatis ad harum repudiandae repellat perferendis, culpa libero molestias, accusamus beatae? Officiis tenetur esse error perspiciatis, similique maxime tempore iure, deleniti, maiores blanditiis consequatur. Perferendis rerum tempora natus exercitationem eius est ipsa sapiente laudantium quis, at porro eaque nulla nemo, dignissimos incidunt aut, praesentium nobis. Perspiciatis inventore non maxime earum deleniti blanditiis excepturi, sapiente alias debitis error praesentium porro eaque accusantium consequatur, eum ratione, eligendi obcaecati sint autem aperiam sed ipsum! Nemo voluptatum alias dolorem aliquam placeat ullam qui. Explicabo odit iusto cumque quod omnis laudantium commodi facilis a saepe? Repudiandae voluptatibus facere quod soluta facilis, sit laboriosam odio! Optio doloribus corrupti laborum nihil ad dolores recusandae harum accusamus laboriosam, eius temporibus, praesentium voluptatum exercitationem reiciendis voluptas. Quae corrupti inventore sed laborum pariatur dolores!</p>
+        <p><?php echo $res['nbody']?></p>
     </div>
+    <?php
+        }
+        ?>
     <footer>
     <p>All Sports SRL - Copyright 2024 MIRA EL ENCARGADO EN DISNEUPUSL PUNTO COM AHORA O TE MATAMOS MANDAMOS U SICARIO A
       TU CASA</p>
